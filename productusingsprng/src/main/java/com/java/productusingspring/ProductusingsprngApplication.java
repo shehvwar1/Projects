dd9af4a87179a4dcb738371685192c3b78ec14e3
subuhi shehvwar <shehvwars16@gmail.com>
2023-01-12 19:41:08 +0530

    Spring with swagger

---
+++ productusingsprng/src/main/java/com/java/productusingspring/ProductusingsprngApplication.java
@@ -0,0 +1,13 @@
+package com.java.productusingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductusingsprngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductusingsprngApplication.class, args);
+	}
+
+}
